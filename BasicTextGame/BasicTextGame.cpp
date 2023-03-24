030fb2c947d5e7b7822241bec7b69b0e5b030db2
Jorge Alejandre <88166019+JayySaucee@users.noreply.github.com>
2023-03-24 11:43:48 -0700

    Add files via upload

--- BasicTextGame/BasicTextGame.cpp
+++ BasicTextGame/BasicTextGame.cpp
@@ -4,96 +4,96 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <chrono>
+#include <cstdio>
 
 using namespace std;
+class Enemy; // forward declaration of the Enemy class
 
 class Player {
 public:
-	int	health;
-	Player(int h) {
-		health = h;
-	}
-	void heal() {
-		int healAmount = rand() % 11 + 15;
-		health += healAmount;
-		cout << "You healed for " << healAmount << "health!" << endl;
-	}
-	void attack(Enemy& e) {
-		int damageOutput = rand() % 11 + 10; // generating a random number between 10 and 20
-		e.health -= damageOutput;
-		cout << "You attacked the Demon Lord for " << damageOutput << " damage! " << endl;
-	}
+    int health;
+    Player(int h) {
+        health = h;
+    }
 
-	
+    void heal() {
+        int amount = rand() % 11 + 15; // generates a random number between 15 and 25
+        health += amount;
+        std::cout << "Player heals for " << amount << " health!" << std::endl;
+    }
+
+    void attack(Enemy& e); // declare the function, but don't define it
 };
 
 class Enemy {
 public:
-	int health;
-	Enemy(int h) {
-		health = h;
-	}
-	
-	void attack(Player& p) {
-		int damageOutput = rand() % 11 + 20; // generating a random number between 20 and 30
-		p.health -= damageOutput;
-		cout << "You were just attacked for " << damageOutput << " damage! " << endl;
-	}
+    int health;
+    Enemy(int h) {
+        health = h;
+    }
+
+    void attack(Player& p) {
+        int damage = rand() % 11 + 10; // generates a random number between 10 and 20
+        p.health -= damage;
+        std::cout << "Enemy attacks Player for " << damage << " damage!" << std::endl;
+    }
 };
 
-void Fight(Player& player, Enemy& enemy) {
+void Player::attack(Enemy& e) {
+    int damage = rand() % 11 + 10; // generates a random number between 10 and 20
+    e.health -= damage;
+    std::cout << "Player attacks Enemy for " << damage << " damage!" << std::endl;
+}
 
-	cout << "..." << endl;
-	cout << "You finally arrived to the demon lord's lair..";
-	cout << "You set out to put an end to this war with one last fight.";
+void fight(Player& player, Enemy& enemy) {
+    while (player.health > 0 && enemy.health > 0) {
+        std::cout << "Press 'a' to attack or 'h' to heal: ";
+        char input = getchar();
+        std::cout << std::endl;
+        if (input == 'a') {
+            player.attack(enemy);
+            std::cout << "Enemy health: " << enemy.health << std::endl << endl << endl;
+            if (enemy.health <= 0) {
+                std::cout << "Player wins!" << std::endl;
+                break;
+            }
+            enemy.attack(player);
+            std::cout << "Player health: " << player.health << std::endl << endl << endl;
+            if (player.health <= 0) {
+                std::cout << "Enemy wins!" << std::endl;
+                break;
+            }
+        }
+        else if (input == 'h') {
+            player.heal();
+            enemy.attack(player);
+            std::cout << "Player health: " << player.health << std::endl << endl;
+            if (player.health <= 0) {
+                std::cout << "Enemy wins!" << std::endl << endl;
+                break;
+            }
+        }
+        
+    }
+}
 
-
-	while (player.health > 0 && enemy.health > 0) {
-		std::cout << "Press 'a' to attack or 'h' to heal: ";
-		char input = getchar();
-		std::cout << std::endl;
-
-		if (input == 'a') {
-			player.attack(enemy);
-			std::cout << "Enemy health: " << enemy.health << std::endl;
-			if (enemy.health <= 0) {
-				std::cout << "Player wins!" << std::endl;
-				break;
-			}
-			enemy.attack(player);
-			std::cout << "Player health: " << player.health << std::endl;
-			if (player.health <= 0) {
-				std::cout << "Enemy wins!" << std::endl;
-				break;
-			}
-		}
-
-		else if (input == 'h') {
-			player.heal();
-			enemy.attack(player);
-			std::cout << "Player health: " << player.health << std::endl;
-			if (player.health <= 0) {
-				std::cout << "Enemy wins!" << std::endl;
-				break;
-			}
-		}
-
-		else {
-			std::cout << "Invalid input. Please try again." << std::endl;
-		}
-	}
+void introduce() {
+    std::cout << "This is a turn based fighting mini game." << endl;
+    std::cout<< "You are about to fight an opponent who has 300HP, and you only have 250HP" << endl;
+    std::cout << "Try to strategize by either healing or attacking when it is your turn." << endl;
+    cout << "Your opponent does more damage on average than you." << endl << endl;
 }
 
 int main() {
-	srand(time(0));
+    srand(time(0)); // seed the random number generator with the current time
 
-	Player	player(250);
-	Enemy	enemy(500);
+    Player player(250);
+    Enemy enemy(300);
 
-	fight(player, enemy);
+    introduce();
+    fight(player, enemy);
 
-	return 0
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
